027db5928d61f4632ab76a0008d4f8a9edf693c9
Deklin Caban <deklin.caban@gmail.com>
2023-04-01 15:56:56 -0400

    Tenative implementation of accelerometer, subject to testing.

--- payload.py
+++ payload.py
@@ -24,6 +24,13 @@ bus = SMBus(1) # initialize a new I2C bus
 bus.write_byte(ADC_REGISTER, 0x10) # Continuous conversion mode, 12 bit resolution
 # ---------
 
+# Accelerometer setup
+# -------------------
+ACCELEROMETER_REGISTER = 0xFA
+ACC_DATA = [0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D] # X, X, Y, Y, Z, Z
+# First byte is 8 MSB, 2nd bit is 4 LSB
+# -------------------
+
 # Function to run ADC
 # -------------------
 def get_voltage():
@@ -33,13 +40,32 @@ def get_voltage():
         voltage -= 4095 # handle overflow
     return voltage
 # -------------------
+
+# Function to run accelerometer
+# -----------------------------
+def get_acc(MSB, LSB):
+    dataLSB = bus.read_byte_data(ACCELEROMETER_REGISTER, LSB)
+    dataMSB = bus.read_byte_data(ACCELEROMETER_REGISTER, MSB)
+    # convert data to 10 bits
+    accl = ((dataMSB & 0x03) * 256) + dataLSB
+    if accl > 511:
+        accl -= 1024
+    return accl
+# -----------------------------
     
 # bool FLIGHT = False # Flight status
 timer = 60
 
+# check acceleration
+acceleration = 0
+while(acceleration <= 5):
+    x_acc = get_acc(ACC_DATA[0],ACC_DATA[1])
+    y_acc = get_acc(ACC_DATA[2],ACC_DATA[3])
+    z_acc = get_acc(ACC_DATA[4],ACC_DATA[5])
+    acceleration = max(x_acc, y_acc, z_acc)
+
 # start recording / capturing voltage data
 # ----------------------------------------
-x = 0
 camera.start_recording(encoder, output)
 with open('voltage_data.csv', "a", newline='') as csv_file:
     while x < timer:
